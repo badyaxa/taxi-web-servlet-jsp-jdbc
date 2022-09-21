4347dab8fbf3d4cbad756cc2ec00cde28bb4a9b0
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 15:55:58 +0300

    join DONE

--- src/main/java/taxi/jdbc/Main.java
+++ src/main/java/taxi/jdbc/Main.java
@@ -1,9 +1,13 @@
 package taxi.jdbc;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import taxi.jdbc.lib.Injector;
+import taxi.jdbc.model.Car;
 import taxi.jdbc.model.Driver;
 import taxi.jdbc.model.Manufacturer;
+import taxi.jdbc.service.CarService;
 import taxi.jdbc.service.DriverService;
 import taxi.jdbc.service.ManufacturerService;
 
@@ -11,45 +15,81 @@ public class Main {
     private static Injector injector = Injector.getInstance("taxi.jdbc");
 
     public static void main(String[] args) {
+        /////////////Manufacturer/////////////
         ManufacturerService manufacturerService = (ManufacturerService)
                 injector.getInstance(ManufacturerService.class);
-        /////create/////
-        Manufacturer toyota = manufacturerService
-                .create(new Manufacturer(null, "Toyota", "Japa"));
-        System.out.println("savedManufacturer = " + toyota);
-        /////get/////
-        Manufacturer manufacturerToyota = manufacturerService.get(toyota.getId());
-        System.out.println("manufacturerToyota = " + manufacturerToyota);
-        /////update/////
-        Manufacturer updateToyota = new Manufacturer(toyota.getId(), "Toyota", "Japan");
-        Manufacturer update = manufacturerService.update(updateToyota);
-        System.out.println("update = " + update);
-        /////delete/////
-        boolean isDeletedToyota = manufacturerService.delete(toyota.getId());
-        System.out.println("deleteToyota = " + isDeletedToyota);
-        /////getAll/////
+        /////create toyota/////
+        Manufacturer newManufacturer = new Manufacturer();
+        newManufacturer.setName("Toyota");
+        newManufacturer.setCountry("Japan");
+        Manufacturer toyota = manufacturerService.create(newManufacturer);
+        System.out.println("createdManufacturer = " + toyota);
+        /////getAll manufacturers/////
         List<Manufacturer> manufacturers = manufacturerService.getAll();
         manufacturers.forEach(System.out::println);
 
         /////////////Driver////////////
         DriverService driverService = (DriverService)
                 injector.getInstance(DriverService.class);
-        /////create/////
-        Driver bohdan = driverService
-                .create(new Driver(null, "Bohdan", "321654987"));
-        System.out.println("savedDriver = " + bohdan);
-        /////get/////
-        Driver driverBohdan = driverService.get(bohdan.getId());
-        System.out.println("driverBohdan = " + driverBohdan);
-        /////update/////
-        Driver updateBohdan = new Driver(bohdan.getId(), "Bohdan", "951753654");
-        Driver updatedDriver = driverService.update(updateBohdan);
-        System.out.println("updatedDriver = " + updatedDriver);
-        /////delete/////
-        boolean isDeletedDriver = driverService.delete(bohdan.getId());
-        System.out.println("deleteDriver = " + isDeletedDriver);
-        /////getAll/////
+        /////create driver/////
+        Driver bohdan = new Driver();
+        bohdan.setName("Bohdan");
+        bohdan.setLicenseNumber(String.valueOf(new Random().nextInt(999999999)));
+        bohdan = driverService.create(bohdan);
+        System.out.println("createdDriver = " + bohdan);
+        /////getAll drivers/////
+        System.out.println("-------getAllDrivers-------");
         List<Driver> drivers = driverService.getAll();
         drivers.forEach(System.out::println);
+
+        /////////////Car////////////
+        CarService carService = (CarService)
+                injector.getInstance(CarService.class);
+        /////create toyota RAV4 /////
+        Car newCarToyotaRav4 = new Car();
+        newCarToyotaRav4.setManufacturer(manufacturerService.get(toyota.getId()));
+        newCarToyotaRav4.setModel("RAV4");
+        newCarToyotaRav4.setDrivers(new HashSet<>(drivers));
+        Car createdCar = carService
+                .create(newCarToyotaRav4);
+        System.out.println("createdCar = " + createdCar);
+        /////get/////
+        Car toyotaRav4 = carService.get(createdCar.getId());
+        System.out.println("getCar = " + toyotaRav4);
+        /////getAll/////
+        System.out.println("-------getAllCars-------");
+        List<Car> cars = carService.getAll();
+        cars.forEach(System.out::println);
+        /////addDriverToCar/////
+        Driver bob = new Driver();
+        bob.setName("Bob");
+        bob.setLicenseNumber(String.valueOf(new Random().nextInt(999999999)));
+        bob = driverService.create(bob);
+        System.out.println("createdDriver = " + bob);
+
+        System.out.println("-------getAllDrivers------");
+        drivers = driverService.getAll();
+        drivers.forEach(System.out::println);
+
+        carService.addDriverToCar(bob, toyotaRav4);
+        System.out.println("toyotaRav4.allDrivers = " + carService.get(toyotaRav4.getId()));
+        carService.addDriverToCar(bob, toyotaRav4);
+        carService.addDriverToCar(bob, toyotaRav4);
+        carService.addDriverToCar(bob, toyotaRav4);
+
+        /////removeDriverFromCar/////
+        System.out.println("toyotaRav4.allDrivers = " + carService.get(toyotaRav4.getId()));
+        carService.removeDriverFromCar(bob, toyotaRav4);
+        System.out.println("toyotaRav4.allDrivers = " + carService.get(toyotaRav4.getId()));
+        carService.removeDriverFromCar(bob, toyotaRav4);
+        System.out.println("toyotaRav4.allDrivers = " + carService.get(toyotaRav4.getId()));
+
+        carService.addDriverToCar(bob, toyotaRav4);
+
+        /////getAllByDriver/////
+        List<Car> bohdanCars = carService.getAllByDriver(bohdan.getId());
+        System.out.println("bohdanCars = " + bohdanCars);
+        List<Car> bobCars = carService.getAllByDriver(bob.getId());
+        System.out.println("bobCars = " + bobCars);
     }
 }
