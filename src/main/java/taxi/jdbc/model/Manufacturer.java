82942be8dec4790a1545cf47c14ea7b1c28857e6
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 13:37:15 +0300

    added Manufacturer

---
+++ src/main/java/taxi/jdbc/model/Manufacturer.java
@@ -0,0 +1,36 @@
+package taxi.jdbc.model;
+
+public class Manufacturer {
+    private Long id;
+    private String name;
+    private String country;
+
+    public Manufacturer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
