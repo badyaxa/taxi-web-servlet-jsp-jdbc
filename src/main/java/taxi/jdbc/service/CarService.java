548094845cb0ff5bc697ac6112fd408ba7672456
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 15:39:36 +0300

    add CarService

---
+++ src/main/java/taxi/jdbc/service/CarService.java
@@ -0,0 +1,23 @@
+package taxi.jdbc.service;
+
+import java.util.List;
+import taxi.jdbc.model.Car;
+import taxi.jdbc.model.Driver;
+
+public interface CarService {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    void addDriverToCar(Driver driver, Car car);
+
+    void removeDriverFromCar(Driver driver, Car car);
+
+    List<Car> getAllByDriver(Long driverId);
+}
