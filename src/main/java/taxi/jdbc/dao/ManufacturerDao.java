dde409f3079669fa73f9971da61aad2b0ce9f065
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 13:37:57 +0300

    added ManufacturerDao

---
+++ src/main/java/taxi/jdbc/dao/ManufacturerDao.java
@@ -0,0 +1,17 @@
+package taxi.jdbc.dao;
+
+import java.util.List;
+import java.util.Optional;
+import taxi.jdbc.model.Manufacturer;
+
+public interface ManufacturerDao {
+    Manufacturer create(Manufacturer manufacturer);
+
+    Optional<Manufacturer> get(Long id);
+
+    List<Manufacturer> getAll();
+
+    Manufacturer update(Manufacturer manufacturer);
+
+    boolean delete(Long id);
+}
