454d59798aae4e990a224cfe1c2aeee28b4e3044
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 17:16:30 +0300

    add ?useUnicode=true&serverTimezone=UTC";

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi?useUnicode=true&serverTimezone=UTC";
     private static final String USERNAME = "badyaxa";
     private static final String PASSWORD = "root";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
