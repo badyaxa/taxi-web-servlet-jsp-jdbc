6f89f3db3f798111bf0d82fed62b15b89c1f6f33
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 15:18:00 +0300

    add data in manufacturers

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,18 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+INSERT INTO `manufacturers` (`id`, `name`, `country`) VALUES
+(1,'Acura','Japan'),
+(2,'Alfa Romeo','Italy'),
+(3,'Audi','Germany'),
+(4,'BMW','Germany'),
+(5,'Ferrari','Italy'),
+(6,'SEAT','Spain'),
+(7,'Renault','France'),
+(8,'Peugeot','France'),
+(9,'Fiat','Italy'),
+(10,'Honda','Japan'),
+(11,'Lexus','Japan');
+
+SELECT * FROM taxi_service_db.manufacturers;
