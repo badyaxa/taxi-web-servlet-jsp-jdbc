d6149517de89e8719c07e38049b0a07ff0a09160
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-21 13:40:06 +0300

    get a resultSet from DB

--- src/main/java/taxi/jdbc/Main.java
+++ src/main/java/taxi/jdbc/Main.java
@@ -1,11 +1,21 @@
-package taxi.jdbc;
+package mate.jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import mate.jdbc.util.ConnectionUtil;
 
 public class Main {
     public static void main(String[] args) {
         Connection connection = ConnectionUtil.getConnection();
-        System.out.println("connection = " + connection);
+        Statement getAllMnufacturersStatement = null;
+        try {
+            getAllMnufacturersStatement = connection.createStatement();
+            ResultSet resultSet = getAllMnufacturersStatement.executeQuery("SELECT * FROM manufacturers");
+            System.out.println("resultSet = " + resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't ", e);
+        }
     }
 }
